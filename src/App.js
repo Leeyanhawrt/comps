7909a01bf7d58788e0b46834475f8980e9d50c55
Leeyanhawrt <leeyan1108@gmail.com>
2023-07-26 16:44:14 -0400

    Learn how to make accordian style

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import { GoBell } from 'react-icons/go'
 function App() {
   const items = [
     {
-      label: "Can I use react on a proeject",
+      id: 1,
+      label: "Can I use react on a project",
       content: "You can use react on any project that you want"
     },
     {
+      id: 2,
       label: "Hello hello test",
       content: "That is content not title"
     },
     {
+      id: 3,
       label: "Last test",
       content: "Is it really now?"
     }
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,23 @@
+import { render } from "@testing-library/react";
+import { useState } from 'react'
+
 function Accordion({ items }) {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    return (
+      <div key={item.id}>
+        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+        {isExpanded && <div>{item.content}</div>}
+      </div>
+    )
+  })
+
   return (
     <div>
-
+      {renderedItems}
     </div>
   )
 }
