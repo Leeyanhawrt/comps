331c31556e1c186639c7a3304352af1b5203cbd5
Leeyanhawrt <leeyan1108@gmail.com>
2023-07-26 17:02:22 -0400

    Finish accordian main feature

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react";
-import { useState } from 'react'
+import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
@@ -7,16 +8,23 @@ function Accordion({ items }) {
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
+    const icon = <span>
+      {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+    </span>
+
     return (
       <div key={item.id}>
-        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
-        {isExpanded && <div>{item.content}</div>}
+        <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => setExpandedIndex(index)}>
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     )
   })
 
   return (
-    <div>
+    <div className="border-t border-x rounded">
       {renderedItems}
     </div>
   )
