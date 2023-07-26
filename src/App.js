565123c9d7766709841e021c8cb37c2b13f4e8ec
Leeyanhawrt <leeyan1108@gmail.com>
2023-07-26 11:00:21 -0400

    Finish working component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import Button from "./Button";
 import { GoBell } from 'react-icons/go'
 
 function App() {
+  const handleClick = () => {
+    console.log("Testing")
+  }
+
   return (
     <div>
-      <div><Button primary rounded outline>Prio 3</Button></div>
+      <div><Button primary rounded outline className="mb-5" onClick={handleClick}>Prio 3</Button></div>
       <div><Button danger>Prio 4</Button></div>
       <div><Button warning outline>Prio 5</Button></div>
       <div><Button success><GoBell />Prio 6</Button></div>
