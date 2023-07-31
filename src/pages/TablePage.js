a41ee8c78929a2d1190aa23d7a358e3c7d797b74
Leeyanhawrt <leeyan1108@gmail.com>
2023-07-30 23:07:06 -0400

    Sort tables using state trackers

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,4 @@
-import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -9,9 +9,9 @@ function TablePage() {
   ];
 
   const config = [
-    { label: 'Name', render: (fruit) => fruit.name },
+    { label: 'Name', render: (fruit) => fruit.name, sortValue: (fruit) => fruit.name },
     { label: 'Color', render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div> },
-    { label: 'Score', render: (fruit) => fruit.score, header: () => <th className="bg-red-500">Score</th> },
+    { label: 'Score', render: (fruit) => fruit.score, sortValue: (fruit) => fruit.score },
   ]
 
   const keyFn = (fruit) => {
@@ -19,7 +19,7 @@ function TablePage() {
   };
 
   return (
-    <Table data={data} keyFn={keyFn} config={config} />
+    <SortableTable data={data} keyFn={keyFn} config={config} />
   )
 }
 
